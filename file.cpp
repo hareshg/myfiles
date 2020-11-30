4c3dcb4402c306fb806664931f540fcaa8895057
Haresh Gaikwad <haresh.03gaikwad@gmail.com>
2020-11-30 08:51:53 +0530

    Pascal Triangle

--- file.cpp
+++ file.cpp
@@ -1,1 +1,25 @@
-ok
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	
+		int n=10,i,j;
+		for(i=1;i<=n;i++){
+			int num=1;
+			for(int spaces = 1;spaces<=(n-i+1);spaces++)
+			      cout << " ";
+			for(j=1;j<=i;j++){
+				if(j==1)
+					cout << j << " ";
+				else{
+					num=num*(i-j+1)/(j-1);
+					cout << num << " ";
+				}
+			}
+			cout << endl;
+		}
+			
+	return 0;
+}
