c923a4bda07172bd5c112dc2157962158a98ea8f
Haresh Gaikwad <haresh.03gaikwad@gmail.com>
2024-05-13 15:43:13 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -29,6 +29,28 @@ void merge_sort(int a[], int s, int e) {
 	merge(a, s, e);
 }
 
+// 2024 
+void mergeSort(vector<int>& nums, int a, int b){
+    if (a >= b) return;
+    int mid = (b+a)/2;
+    mergeSort(nums, a, mid);
+    mergeSort(nums, mid+1, b);
+    
+    // merge both part
+    int i = a, j = mid+1;
+    vector<int> t;
+    while(i <= mid and j <= b){
+        if(nums[i] < nums[j]) t.push_back(nums[i++]);
+        else t.push_back(nums[j++]);
+    }
+    while(i <= mid) t.push_back(nums[i++]);
+    while(j <= b) t.push_back(nums[j++]);
+    int k = a;
+    for(int i = 0; i < t.size(); i++){
+        nums[k++] = t[i];
+    }
+}
+
 int main() {
 	int a[] = {2, 1, 3, 6, 4, 0, 5, 3, 3, 3, 3, 56, 3, 3};
 	int size = sizeof(a) / sizeof(a[0]);
